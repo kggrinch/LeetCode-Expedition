e8997aedd4d0d1a6b3a850da2a14760361434fa4
Kirill <kirillkongrichanichenko@gmail.com>
2025-09-15 18:15:11 -0700

    solved findmedianfromdatastream

--- src/Hard/FindMedianFromDataStream/main.cpp
+++ src/Hard/FindMedianFromDataStream/main.cpp
@@ -3,16 +3,30 @@
 
 int main()
 {
-
     MedianFinder* obj = new MedianFinder();
-    std::cout << "obj->addNum(1) \n";
-    obj->addNum(1);
+    std::cout << "obj->addNum(-1) \n";
+    obj->addNum(-1);
+
     std::cout << "findMedian: " << obj->findMedian() << "\n";
-    std::cout << "obj->addNum(3) \n";
-    obj->addNum(3);
+
+    std::cout << "obj->addNum(-2) \n";
+    obj->addNum(-2);
+
     std::cout << "findMedian: " << obj->findMedian() << "\n";
-    std::cout << "obj->addNum(2) \n";
-    obj->addNum(2);
+
+    std::cout << "obj->addNum(-3) \n";
+    obj->addNum(-3);
+
+    std::cout << "findMedian: " << obj->findMedian() << "\n";
+
+    std::cout << "obj->addNum(-4) \n";
+    obj->addNum(-4);
+
+    std::cout << "findMedian: " << obj->findMedian() << "\n";
+
+    std::cout << "obj->addNum(-5) \n";
+    obj->addNum(-5);
+
     std::cout << "findMedian: " << obj->findMedian() << "\n";
 
     return 0;
