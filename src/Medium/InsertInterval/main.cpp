e19ec091a71e3f9ae73772d2947c98d4a2ce055d
Kirill <kirillkongrichanichenko@gmail.com>
2025-10-03 12:41:53 -0700

    solved insertInterval

--- src/Medium/InsertInterval/main.cpp
+++ src/Medium/InsertInterval/main.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <vector>
 
+// Time Complexity: O(n) where n = each index pair
+// Space Complexity: O(n) where n = each index pair
 std::vector<std::vector<int>> insert(std::vector<std::vector<int>>& intervals, std::vector<int>& newInterval)
 {
-    std::vector<std::vector<int>> result;
+    std::vector<std::vector<int>> result; // build result
+    if (intervals.empty()) {result.push_back(newInterval); return result;} // if interval empty return new interval
+    // Iterate through each interval index
     for (int i = 0; i < intervals.size(); ++i)
     {
-        if (newInterval[1] < intervals[i][0]) result.push_back(newInterval);
-        else if (newInterval[0] > intervals[i][1]) result.push_back(newInterval);
+        // If newIntervals max value is less than current intervals lowest value
+        // place new interval in result and update new interval to be the current interval index
+        if (newInterval[1] < intervals[i][0])
+        {
+            result.push_back(newInterval);
+            newInterval = intervals[i];
+        }
+        // If newIntervals the lowest value is greater than current intervals greatest value
+        // place current interval in results and continue to next interval index
+        else if (newInterval[0] > intervals[i][1]) result.push_back(intervals[i]);
+        // If newInterval merges with current interval then update the new interval
+        // between the lowest and largest values in both and continue to next interval
         else
         {
             newInterval[0] = std::min(newInterval[0], intervals[i][0]);
             newInterval[1] = std::max(newInterval[1], intervals[i][1]);
         }
     }
+    // After the loop ends the newInterval will contain interval that belongs at the end of the list.
     result.push_back(newInterval);
     return result;
 }
