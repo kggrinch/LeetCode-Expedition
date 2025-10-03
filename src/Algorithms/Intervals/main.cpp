a84dd761f8f7b1856cde358a2609eb757ca48430
Kirill <kirillkongrichanichenko@gmail.com>
2025-10-03 12:57:40 -0700

    created intervals algorithm notes

---
+++ src/Algorithms/Intervals/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // Intervals - Pairs of values indicating the [start, end]. Intervalues come in a list of pairs which then require some sort of solution.
+    // Example: [[1, 3], [6, 9]] - this indicates all of these values [1, 2, 3, 6, 7, 8, 9]
+    //          A problem can then be something like insert interval [4, 7] into the interval above
+
+    return 0;
+}
