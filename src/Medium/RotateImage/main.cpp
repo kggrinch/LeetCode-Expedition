2d79a90ba928e960ba79ae3a5a60bf3888f26bc2
Kirill <kirillkongrichanichenko@gmail.com>
2025-10-13 15:57:27 -0700

    solved rotate image

--- src/Medium/RotateImage/main.cpp
+++ src/Medium/RotateImage/main.cpp
@@ -2,23 +2,69 @@
 #include <vector>
 #include <algorithm>
 
+// Time Complexly: O(n^2)
+// Space Complexity: O(1)
 void rotate(std::vector<std::vector<int>>& matrix)
 {
-    int row = matrix.size();
-    int column = matrix[0].size();
-    int i = 0;
-    int j = column;
+    int l = 0;
+    int r = matrix[0].size() - 1;
 
-    // Reverse each
-    for (int i = 0; i < row; i++)
+    // Increment to inner rectangle
+    while (l <= r)
     {
-       std::reverse(matrix[i].begin(), matrix[i].end());
+        // rotate n times
+        for (int i = 0; i < r - l; i++)
+        {
+            int t = l;
+            int b = r;
+
+            int top_left = matrix[t][l + i];
+
+            // Move bottom left to top left
+            matrix[t][l + i] = matrix[b - i][l];
+
+            // Move bottom right to bottom left
+            matrix[b - i][l] = matrix[b][r - i];
+
+            // Move top right to bottom right
+            matrix[b][r - i] = matrix[t + i][r];
+
+            // Move top left to top right
+            matrix[t + i][r] = top_left;
+        }
+        l++;
+        r--;
     }
+}
 
-
+void print_matrix(const std::vector<std::vector<int>>& matrix)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[0].size(); j++)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
 }
 
 int main()
 {
+    std::vector<std::vector<int>> matrix =
+    {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9},
+    };
+    std::cout << "Before:\n";
+    print_matrix(matrix);
+
+    rotate(matrix);
+
+    std::cout << "\nAfter:\n";
+    print_matrix(matrix);
+
+
     return 0;
 }
