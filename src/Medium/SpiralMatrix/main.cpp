8fd5986efbb588e1defc0a5b7b92b50abf50e515
Kirill <kirillkongrichanichenko@gmail.com>
2025-10-16 14:39:19 -0700

    solved spiral matrix

--- src/Medium/SpiralMatrix/main.cpp
+++ src/Medium/SpiralMatrix/main.cpp
@@ -1,30 +1,66 @@
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
+// Time Complexity: O(n*m) where n = rows and m = columns
+// Space Complexity: O(1)
 std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix)
 {
-    int c = matrix.size();
-    int r = matrix[0].size();
     std::vector<int> result;
-    std::vector<int> visited(c * r, -1);
-    for (int i = 0; i < matrix.size(); i++)
+    int l = 0;
+    int r = matrix[0].size();
+    int t = 0;
+    int b = matrix.size();
+    while (l < r && t < b)
     {
-        // Read the left to right
-        for (int j = 0; j < matrix[0].size(); j++)
+        // cover the top values (left to right) (top pointer)
+        for (int i = t; i < r; ++i)
         {
-           int value = matrix[i][j];
-           if (result[value] != -1)
-           {
-               visited[value] = value;
-               result.push_back(value);
-           }
+            result.push_back(matrix[t][i]);
         }
+        t++; // increment top to move it down
+
+        // cover right values (top to bottom) (right pointer)
+        for (int i = t; i < b; ++i)
+        {
+            result.push_back(matrix[i][r - 1]);
+        }
+        r--; // decrement right to move it left
+
+        // Edge case handling
+        // if l passed r of if t passed b then
+        // the row or column we just left, and we don't need to go further down and can break.
+        if (l >= r || t >= b) break;
+
+        // cover bottom values (right to left) (bottom pointer)
+        for (int i = r - 1; i >= l; --i)
+        {
+            result.push_back(matrix[b - 1][i]);
+        }
+        b--; // decrement bottom to move it up
+
+        // cover left values (bottom to top) (left pointer)
+        for (int i = b - 1; i >= t; --i)
+        {
+            result.push_back(matrix[i][l]);
+        }
+        l++; // increment left to move it right
     }
+    return result;
 }
 
 int main()
 {
+    std::vector<std::vector<int>> matrix
+    {
+        {1},
+        {2},
+        {3},
+        {4}
+    };
 
+    // {1,2,3,4}
+    std::vector<int> result = spiralOrder(matrix);
 
     return 0;
 }
